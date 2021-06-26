e1af815567447d14473024ecc43bbae6f9023c1b
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2021-06-26 17:00:02 -0300

    feat: ajustando método watch

--- src/components/CompositionApi.vue
+++ src/components/CompositionApi.vue
@@ -2,25 +2,33 @@
   <p>Variável: {{ reactiveVariable }}</p>
   <input v-model="reactiveVariable" />
   <p>{{ users }}</p>
+  <p>Número de usuários: {{ numberOfUsers }}</p>
 </template>
 
 <script>
 import { ref } from "@vue/reactivity";
 import UserService from "@/services/UserService";
-import { onMounted } from "@vue/runtime-core";
+import { onMounted, watch } from "@vue/runtime-core";
 export default {
   setup() {
     const reactiveVariable = ref("initial value");
     const users = ref([]);
+    const numberOfUsers = ref(0);
     const userService = new UserService();
 
     const getUsers = async () => {
       users.value = await userService.list();
     };
 
+    const updateNumberOfUsers = (newUsersValue) => {
+      numberOfUsers.value = newUsersValue.length;
+    };
+
     onMounted(getUsers);
 
-    return { reactiveVariable, users };
+    watch(users, updateNumberOfUsers);
+
+    return { reactiveVariable, users, numberOfUsers };
   },
 };
 </script>
